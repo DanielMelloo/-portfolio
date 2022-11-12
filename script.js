570ce744be1bc20faa99678c2addb3c0f55f1326
Daniel Mello <danielmello.nfa@gmail.com>
2022-11-12 01:57:58 -0300

    Principal Funcional

--- script.js
+++ script.js
@@ -62,3 +62,43 @@ const observer = new IntersectionObserver ((entries) => {
 // const hiddenSection = document.querySelectorAll('.principal-section-hidden');
 const hiddenSection = document.querySelectorAll('.hidden');
 hiddenSection.forEach((el) => observer.observe(el));
+
+// function rolling (){
+//   let obj=event.target.dataset.nome;
+//   document.getElementById (obj).scrollIntoView();
+// }
+
+// function init()
+// {
+//   document.getElementById('intro').addEventListener('click', rolling);
+//   document.getElementById('aboutme').addEventListener('click', rolling);
+//   document.getElementById('habilities').addEventListener('click', rolling);
+//   // document.getElementById('btn').addEventListener('click', rolling);
+// }
+
+// window.addEventListener('load', init);
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
