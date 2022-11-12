966ac61c54787bef0d1bb4fda27a120fa237cd68
Daniel Mello <danielmello.nfa@gmail.com>
2022-11-12 02:17:43 -0300

    Smooth Behavior

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ toggleButton.addEventListener('click', () => {
 });
 
 
+// ================= //
+// Section Animation //
+// ================= //
 
 function ativaNoScroll() {
 
@@ -59,46 +62,33 @@ const observer = new IntersectionObserver ((entries) => {
   });
 });
 
+
 // const hiddenSection = document.querySelectorAll('.principal-section-hidden');
 const hiddenSection = document.querySelectorAll('.hidden');
 hiddenSection.forEach((el) => observer.observe(el));
 
-// function rolling (){
-//   let obj=event.target.dataset.nome;
-//   document.getElementById (obj).scrollIntoView();
-// }
 
-// function init()
-// {
-//   document.getElementById('intro').addEventListener('click', rolling);
-//   document.getElementById('aboutme').addEventListener('click', rolling);
-//   document.getElementById('habilities').addEventListener('click', rolling);
-//   // document.getElementById('btn').addEventListener('click', rolling);
-// }
+// ============== //
+// Scroll To View //
+// ============== //
 
-// window.addEventListener('load', init);
+  
+document.documentElement.style.scrollBehavior = "smooth";
 
 $(document).ready(function(){
-  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 0, function(){
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
