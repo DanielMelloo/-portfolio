fac6ead5eec2796dcead0851cbb23b8a23c8e26e
Daniel Mello <danielmello.nfa@gmail.com>
2023-02-14 01:22:23 -0300

    Correções e mudanças

--- script.js
+++ script.js
@@ -1,93 +1,63 @@
+window.onload = function () {
+    Particles.init({
+        selector: '.background'
+    });
+};
 
+
+let headerMenu = document.getElementById('header-menu');
 
 
 $(function () {
-  $(".menu-link").click(function () {
-    $(".menu-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
+    $(".menu-link").click(function () {
+        $(".menu-link").removeClass("is-active");
+        $(this).addClass("is-active");
+    });
 });
 
 $(function () {
-  $(".main-header-link").click(function () {
-    $(".main-header-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
+    $(".main-header-link").click(function () {
+        $(".main-header-link").removeClass("is-active");
+        $(this).addClass("is-active");
+    });
 });
 
 $(".search-bar input")
-  .focus(function () {
-    $(".header").addClass("wide");
-  })
-  .blur(function () {
-    $(".header").removeClass("wide");
-  });
+    .focus(function () {
+        $(".header").addClass("wide");
+    })
+    .blur(function () {
+        $(".header").removeClass("wide");
+    });
 
 
 
 // =============== //
 // Light-Dark Mode //
-
 // =============== //
 
-// const toggleButton = document.querySelector('.dark-light');
+const toggleButton = document.querySelector('.dark-light');
 
-if (localStorage.getItem ('lightMode') == 1){
-
-    document.body.classList.add('light-mode');
-
-    // document.body.classList.toggle('light-mode');
-    // alert ('light')
-}
-
-else if (localStorage.getItem ('lightMode') == 0){
-
-    document.body.classList.remove('light-mode');
-
-    // document.body.classList.toggle('light-mode');
-    // alert ('dark')
-}
-
-function toggleButtonFunc (){
-
-    if (localStorage.getItem ('lightMode') == 1){
-        localStorage.setItem("lightMode", 0);
-
-        document.body.classList.remove('light-mode');
-
-        console.log ('dark')
-
-        // document.body.classList.toggle('light-mode');
-    }
-
-    else if (localStorage.getItem ('lightMode') == 0){
-
-        localStorage.setItem("lightMode", 1);
-        
-        document.body.classList.add('light-mode');
-        
-        console.log ('light')
-
-        // document.body.classList.toggle('light-mode');
-    }    
-}
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+});
 
 
 
 
 // toggleButton.addEventListener('click', () => {
-    
+
 //     if (localStorage.getItem ('lightMode') == 1){
 
 //         localStorage.setItem("lightMode", 0);
-        
+
 //         document.body.classList.toggle('light-mode');
 //         alert ('dark')
-        
+
 //     }
-    
+
 //     else if (localStorage.getItem ('lightMode') == 0){
-        
+
 //         localStorage.setItem("lightMode", 1);
 
 //         document.body.classList.toggle('light-mode');
@@ -96,68 +66,84 @@ function toggleButtonFunc (){
 // });
 
 
-// ================= //
-// Section Animation //
-// ================= //
-
-function ativaNoScroll() {
-
-  document.querySelectorAll('img').forEach((img, index) => {
-    if(img.getBoundingClientRect().top < window.innerHeight) {
-      img.src = img.getAttribute('data-src');
-    };
-  })
-
-}
-
-window.addEventListener('scroll', ativaNoScroll);
-
 // ============ //
 // Show section //
 // ============ //
 
-const observer = new IntersectionObserver ((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting){
-      // entry.target.classList.add('principal-show-section');
-      entry.target.classList.add('show');
-    }
-    else{
-      // entry.target.classList.remove('principal-show-section');
-      entry.target.classList.remove('show');
-    }
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
 
-  });
+        if (entry.isIntersecting) {
+            // entry.target.classList.add('principal-show-section');
+            entry.target.classList.add('show');
+
+            switch (entry.target.id) {
+
+                case 'intro':
+
+                    for (let child of headerMenu.children) {
+                        
+                        if ( child.getAttribute('href').substring(1) == 'intro') {
+
+                            child.classList.add('is-active'); 
+                        }
+                        
+                        else{
+                            child.classList.remove('is-active'); 
+                        }
+                    }
+
+                    break;
+
+                case 'aboutme':
+
+                    for (let child of headerMenu.children) {
+                        
+                        if ( child.getAttribute('href').substring(1) == 'aboutme') {
+
+                            child.classList.add('is-active'); 
+                        }
+
+                        else{
+                            child.classList.remove('is-active'); 
+                        }
+                    }
+
+                    break;
+
+                case 'habilities':
+
+                    for (let child of headerMenu.children) {
+                        
+                        if ( child.getAttribute('href').substring(1) == 'habilities') {
+
+                            child.classList.add('is-active'); 
+                        }
+
+                        else{
+                            child.classList.remove('is-active'); 
+                        }
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        } else {
+            // entry.target.classList.remove('principal-show-section');
+            entry.target.classList.remove('show');
+        }
+
+    });
 });
 
 
 // const hiddenSection = document.querySelectorAll('.principal-section-hidden');
 const hiddenSection = document.querySelectorAll('.hidden');
 hiddenSection.forEach((el) => observer.observe(el));
-
-
-// ============== //
-// Scroll To View //
-// ============== //
-
-
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-
-    if (this.hash !== "") {
-      event.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 0, function(){
-
-        window.location.hash = hash;
-      });
-    } 
-  });
-});
 
 
 
@@ -167,22 +153,22 @@ $(document).ready(function(){
 
 
 
-let description0 = document.getElementById ('description0');
+let description0 = document.getElementById('description0');
 
 
-function showText (elementHover) {
+function showText(elementHover) {
     let id = parseInt(elementHover.id.slice(7), 10);
-    let elementToDisplay = document.getElementById ('description' + id);
+    let elementToDisplay = document.getElementById('description' + id);
 
     elementToDisplay.setAttribute('style', 'display: block');
-    description0.setAttribute ('style', 'display: none');
+    description0.setAttribute('style', 'display: none');
 }
 
 
-function resetText (elementHoverOff){
+function resetText(elementHoverOff) {
     let id = parseInt(elementHoverOff.id.slice(7), 10);
-    let elementToDisplayOff = document.getElementById ('description' + id);
+    let elementToDisplayOff = document.getElementById('description' + id);
 
     elementToDisplayOff.setAttribute('style', 'display: none');
-    description0.setAttribute ('style', 'display: block');
+    description0.setAttribute('style', 'display: block');
 }
